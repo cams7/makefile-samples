5fa595c6b30d7165e8b3292236779c1b797b482b
César Magalhães <ceanma@gmail.com>
2015-07-17 09:47:17 -0300

    Foi criado o arquivo 'common.mk'

--- arduino.mk
+++
@@ -1,4 +0,0 @@
-ARDUINO_DIR = C:/Users/cams7/desenv/embarcado/arduino/arduino-1.0.6
-ARDUINO_VARIANTS_DIR = $(ARDUINO_DIR)/hardware/arduino/variants/standard
-ARDUINO_CORES_DIR = $(ARDUINO_DIR)/hardware/arduino/cores/arduino
-ARDUINO_CORES_LIBC_DIR = $(ARDUINO_CORES_DIR)/avr-libc
