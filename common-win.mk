91072c198f1cc7f0694445153fcada480f2bbb97
César Magalhães <ceanma@gmail.com>
2015-07-19 16:19:30 -0300

    O arduino foi alterado

--- common-win.mk
+++ common-win.mk
@@ -2,10 +2,9 @@ AVRDUDE_PROGRAMMER=arduino
 AVRDUDE_PORT=COM3
 AVRDUDE_SPEED=115200
 
-ARDUINO_DIR = C:/Users/cams7/desenv/embarcado/arduino/arduino-1.0.6
-ARDUINO_VARIANTS_DIR = $(ARDUINO_DIR)/hardware/arduino/variants/standard
-ARDUINO_CORES_DIR = $(ARDUINO_DIR)/hardware/arduino/cores/arduino
-ARDUINO_CORES_LIBC_DIR = $(ARDUINO_CORES_DIR)/avr-libc
+ARDUINO_DIR = C:/Users/cams7/desenv/embarcado/arduino/arduino-1.6.5-r2
+ARDUINO_VARIANTS_DIR = $(ARDUINO_DIR)/hardware/arduino/avr/variants/standard
+ARDUINO_CORES_DIR = $(ARDUINO_DIR)/hardware/arduino/avr/cores/arduino
 
 #SILENT ?= @
 CROSS ?= avr-
